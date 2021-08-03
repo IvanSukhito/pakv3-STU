f5ddda69c10e1487e69da6aa23f396ab6ad4a72f
ronx38 <xjinxprohard@gmail.com>
2021-08-03 19:35:49 +0700

    Update web

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,9 @@ Route::group(['prefix' => '/', 'middleware'=>['web']], function () use ($router)
                 'Admin\SettingsController' => 'settings',
                 'Admin\AdminController' => 'admin',
                 'Admin\RoleController' => 'role',
+                'Admin\GolonganController' => 'golongan',
+                'Admin\UnitKerjaController' => 'unit-kerja',
+                'Admin\PendidikanController' => 'pendidikan',
 
                 'Admin\PermenController' => 'permen',
                 'Admin\MsKegiatanController' => 'mskegiatan',
