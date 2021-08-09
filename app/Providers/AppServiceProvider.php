95a9144f06c8d11975182439fa9c8bcfabef1a8a
Hendra PC <hendratjua@gmail.com>
2021-08-09 09:47:51 +0700

    membuat pengajuan surat pernyataan

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         Schema::defaultStringLength(191);
+        Builder::defaultStringLength(191);
     }
 
     /**
