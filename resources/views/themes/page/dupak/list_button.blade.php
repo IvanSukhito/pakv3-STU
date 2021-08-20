7aa825106a018ef5ffff71d01ddf8036ab8d6812
IvanSukhito <sukhitoo.ivan@gmail.com>
2021-08-20 20:12:34 +0700

    update dupak

--- resources/views/themes/page/dupak/list_button.blade.php
+++ resources/views/themes/page/dupak/list_button.blade.php
@@ -5,13 +5,6 @@
         <span class="d-none d-md-inline"> @lang('general.show')</span>
     </a>
 @endif
-@if ($permission['edit'])
-    <a href="{{ route('admin.' . $thisRoute . '.edit', $query->{$masterId}) }}" class="mb-1 btn btn-primary btn-sm"
-       title="@lang('general.edit')">
-        <i class="fa fa-pencil"></i>
-        <span class="d-none d-md-inline"> @lang('general.edit')</span>
-    </a>
-@endif
 @if ($permission['destroy'])
     <a href="#" class="mb-1 btn btn-danger btn-sm" title="@lang('general.delete')"
        onclick="return actionData('{{ route('admin.' . $thisRoute . '.destroy', $query->{$masterId}) }}', 'delete')">
